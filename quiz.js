64ac58e833f2651d3d58027a142c209c73c59c0b
Justin <justinmshyaka@gmail.com>
2025-02-17 21:17:37 +0200

    tried to correct everthing

--- quiz.js
+++ quiz.js
@@ -1,16 +1,11 @@
 function checkAnswer() {
-    var correctAnswer ="4";
-    var userAnswer = document.querySelector('input[name="quiz"]:checked');
+    var correctAnswer = "4";
+    var userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
     var feedback = document.getElementById('feedback');
-
-    if(userAnswer) {
-        if(userAnswer.value === correctAnswer) {
-            feedback.textContent ="Correct! Well done.";
-        } else {
-            feedback.textContent = "That's incorrect. Try again.";
-        }
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
     } else {
-        feedback.textContent ="Please select an ansuer ";
+        feedback.textContent = "That's incorrect. Try again.";
     }
 }
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
